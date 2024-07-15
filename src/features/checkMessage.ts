c4df4ac29c6e33e73797a4d7841a6c7d5e180ff5
Saivelin <vlad.pochkalov@mail.ru>
2024-07-15 19:11:54 +0300

    check message

--- src/features/checkMessage.ts
+++ src/features/checkMessage.ts
@@ -2,7 +2,6 @@ import { Socket } from 'socket.io-client'
 import { StorageUserType } from './useGetLocalStorageUser'
 
 const checkMessage = ({ socket, id }: { socket: Socket; id: number }) => {
-    console.log(id)
     socket.emit('checkMessage', {id: id})
 }
 
